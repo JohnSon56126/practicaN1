7a1f47b7d87eead0209e1ad2dbdfd9fc05af0ea0
JohnSon56126 <evg.pol.mkp@gmail.com>
2022-10-08 11:08:53 +0300

    first commit

---
+++ Practica/Practica/src/main/kotlin/Main.kt
@@ -0,0 +1,43 @@
+import kotlin.math.*
+fun main(args: Array<String>) {
+    // Задание 1
+    var exercises: Int = 13
+    var exercisesSolved: Int = 2
+    var age = 5
+    println(age)
+    // Задание 2
+    var myAge = 18
+    println(myAge)
+    // Задание 3
+    var Age: Double = 18.0
+    var averageAge = (18 + 30) / 2
+    println(averageAge)
+    // Задание 4
+    var testNumber = 8
+    var evenOdd = testNumber % 2
+    println(evenOdd)
+    // Задание 5
+    var answer: Int = 0
+    answer = +1
+    answer = +10
+    answer *= 10
+    answer = answer shr 3
+    println(answer)
+    // Задание 6
+    age = 16
+    println(age)
+    age = 30
+    println(age)
+    //Задание 7
+    val a: Int = 46
+    val b: Int = 10
+    val answer1: Int = a * 100 + b
+    print(answer1)
+    val answer2: Int = (a * 100) + (b * 100)
+    println(answer2)
+    val answer3: Int = (a * 100) + (b / 10)
+    print(answer3)
+    // Задание 8
+
+}
+
